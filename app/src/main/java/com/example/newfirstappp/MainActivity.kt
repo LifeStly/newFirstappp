d50ec6db5d9e638fa02c6ac614aafe16e3884275
LifeStly <tattonman001@gmail.com>
2020-08-23 14:04:05 +0700

    navigateion

--- app/src/main/java/com/example/newfirstappp/MainActivity.kt
+++ app/src/main/java/com/example/newfirstappp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.newfirstappp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,NavigationView.OnNavigationItemSelectedListener{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
