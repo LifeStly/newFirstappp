9545af1a51fed3c57b36512d874c7587f6fa17f3
LifeStly <tattonman001@gmail.com>
2020-08-23 15:18:06 +0700

    Dao

--- app/src/main/java/com/example/newfirstappp/data/Database.kt
+++ app/src/main/java/com/example/newfirstappp/data/Database.kt
@@ -1,7 +1,31 @@
 package com.example.newfirstappp.data
 
+import android.content.Context
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 abstract class Database : RoomDatabase(){
 
+    abstract val databaseDao: DatabaseDao
+    companion object{
+        private var INSTANCE: Database? = null
+
+        fun  getInstance(context: Context): Database{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        Database::class.java,
+                        "contact_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/newfirstappp/data/DatabaseDao.kt
+++ app/src/main/java/com/example/newfirstappp/data/DatabaseDao.kt
@@ -1,4 +1,16 @@
 package com.example.newfirstappp.data
 
+import android.provider.ContactsContract
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface DatabaseDao {
+    @Insert
+    fun insert(contact: contactData)
+
+    @Query("select * from table_contact order by contactId DESC")
+    fun getName(): LiveData<List<contactData>>
 }
